552a57c1c89fa9fd4ee2988203018603d2155ea8
Alexandr Strelets <alexandrstriii@gmail.com>
2025-02-26 13:17:16 +0200

    v2

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -2,15 +2,16 @@
 
 function convertToObject(styles) {
   return styles
-    .replace(/\n/g, '') // Удаляем все переводы строк
-    .split(';') // Разбиваем строку на массив по ;
-    .map(rule => rule.trim()) // Очищаем пробелы вокруг
-    .filter(rule => rule) // Убираем пустые строки
+    .split(';') // Разбивает строку на массив по ;
+    .map((rule) => rule.trim()) // Очищает пробелы вокруг
+    .filter((rule) => rule) // пустые строки
     .reduce((acc, rule) => {
       const [property, ...valueParts] = rule.split(':');
+
       if (property && valueParts.length) {
         acc[property.trim()] = valueParts.join(':').trim();
       }
+
       return acc;
     }, {});
 }
