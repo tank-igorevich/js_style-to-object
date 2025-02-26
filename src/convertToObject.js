ab7a781dfab6eb2f26b6f80b081cad53c99d8020
Alexandr Strelets <alexandrstriii@gmail.com>
2025-02-26 12:48:24 +0200

    v1

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,12 +1,18 @@
 'use strict';
 
-/**
- * @param {string} sourceString
- *
- * @return {object}
- */
-function convertToObject(sourceString) {
-  // write your code here
+function convertToObject(styles) {
+  return styles
+    .replace(/\n/g, '') // Удаляем все переводы строк
+    .split(';') // Разбиваем строку на массив по ;
+    .map(rule => rule.trim()) // Очищаем пробелы вокруг
+    .filter(rule => rule) // Убираем пустые строки
+    .reduce((acc, rule) => {
+      const [property, ...valueParts] = rule.split(':');
+      if (property && valueParts.length) {
+        acc[property.trim()] = valueParts.join(':').trim();
+      }
+      return acc;
+    }, {});
 }
 
 module.exports = convertToObject;
